9c1aba1490212a0cab81b720fa9f22041d3dc5df
kunq <Kun_Qian@msn.com>
2016-07-26 06:09:56 -0400

    comment

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var user_id = req.param('id');
-
-  res.render('index', { title: 'Express' , userid: user_id});
+  //var user_id = req.param('id');
+  res.render('index', { title: 'Express'});
 });
 
 router.post('/', function(req, res) {
   var sizes = req.body.sizes;
-
-
-  /* Here you can store checked value in an Array or Json object instead of String: */
-  var sizesString= '';
-  for (var i=0; i < sizes.length; i++) {
-  	sizesString += sizes[i] + '<br />';
-  }
-  res.send('You have selected the following sizes: <br />' + sizesString);
-
+  if (sizes === undefined) {
+     res.send('Please select a size.');
+  }  
+  else {
+    /* Here you can store checked value in an Array or Json object instead of String: */
+    var sizesString= '';
+    for (var i=0; i < sizes.length; i++) {
+    	sizesString += sizes[i] + '<br />';
+    }
+      res.send('You have selected the following sizes: <br />' + sizesString);
+    }
 });
 
 
